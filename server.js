765894939ddc5875c388cbe1d7fb5f976a936eba
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-30 10:15:31 -0400

    adds port 8080 over 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const port = 3001;
+const port = 8080;
 
 mongoose
    .connect(process.env.MONGO_DB, {
