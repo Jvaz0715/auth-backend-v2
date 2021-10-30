8ca77eb4b705750487247a06cf50a5436da2f849
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-30 13:23:55 -0400

    Handles Errors for invalid URLS

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ const cors = require("cors"); //cors foregoes the blocking
 
 const app = express();
 
+const ErrorMessageHandlerClass = require("./routes/utils/ErrorMessageHandlerClass.js");
 const userRouter = require("./routes/user/userRouter");
 
 app.use(cors());
-app.use(logger("dev"));
+
+// to differentiate between production mode (what client sees) and developer mode with errors
+if(process.env.NODE_ENV === "development") {
+   app.use(logger("dev"));
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // <--- this parses the incoming data
 
 // this is where the app will use whatever url address we create with user router
 app.use("/api/users", userRouter);
+
+// catch all for everything that does not work in URL if it doesn't exist
+// make sure to put it at the bottom!
+app.all("*", function(req, res, next) {
+   next(
+      new ErrorMessageHandlerClass(
+         `Cannot find ${req.originalUrl} on this server! Check your URL`, 
+         404
+      )
+   );
+});
 
 module.exports = app;
---
+++ routes/utils/ErrorMessageHandlerClass.js
@@ -0,0 +1,12 @@
+// Error class comes from node
+class ErrorMessageHandlerClass extends Error {
+   constructor(message, statusCode) {
+      super(message, statusCode);
+
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+      this.isOperational = true;
+   }
+}
+
+module.exports = ErrorMessageHandlerClass;
